2351a30cc31997187d9d6240675967280caf5601
Angelo Ocana <alanmarcell@live.com>
2017-03-04 17:14:42 +0000

    Created index.ts

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.EntityMinBase = exports.EntityBase = exports.validateEmail = undefined;
+
+var _Email = require('./Email');
+
+var _EntityMinBase = require('./EntityMinBase');
+
+var _EntityMinBase2 = _interopRequireDefault(_EntityMinBase);
+
+var _EntityBase = require('./EntityBase');
+
+var _EntityBase2 = _interopRequireDefault(_EntityBase);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.validateEmail = _Email.validateEmail;
+exports.EntityBase = _EntityBase2.default;
+exports.EntityMinBase = _EntityMinBase2.default;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {validateEmail} from './Email';
+import EntityMinBase from './EntityMinBase';
+import EntityBase from './EntityBase';
+
+export{
+    validateEmail,
+    EntityBase,
+    EntityMinBase
+}
