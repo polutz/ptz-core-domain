0fcf002d37cb8765038ba8284f2d9f7bb36ce98f
Allan Egidio <allan.egidio@outlook.com>
2017-03-09 16:43:49 -0300

    Clean typescript errors

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
+/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="modules/bcryptjs/index.d.ts" />
-/// <reference path="modules/dotenv/index.d.ts" />
 /// <reference path="modules/shortid/index.d.ts" />
 /// <reference path="modules/sinon/index.d.ts" />
+/// <reference path="../node_modules/ptz-assert/src/typings/index.d.ts" />
