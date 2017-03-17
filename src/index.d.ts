03c4940bd0286506add6fb7b6ee634b1b43397aa
Ângelo Ocanã <angeloocana@gmail.com>
2017-03-17 20:51:43 -0300

    Testing typescript import from other projects

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,32 +1,26 @@
-import {IEntityMinBase, IEntityMinBaseArgs} from './IEntityMinBase';
-import {IEntityBase, IEntityBaseArgs} from './IEntityBase';
-import {ICreatedBy} from './ICreatedBy';
+import { IEntityMinBase, IEntityMinBaseArgs } from './IEntityMinBase';
+import { IEntityBase, IEntityBaseArgs } from './IEntityBase';
+import { ICreatedBy } from './ICreatedBy';
 
-declare module ptzCoreDomain {
-    class EntityMinBase implements IEntityMinBase {
-        constructor(entity?: IEntityMinBaseArgs);
+class EntityMinBase implements IEntityMinBase {
+    constructor(entity?: IEntityMinBaseArgs);
 
-        id: string;
-        errors: string[];
+    id: string;
+    errors: string[];
 
-        isValid(): boolean;
-        throwErrorIfIsInvalid(): void;
-    }
-
-    class EntityBase implements IEntityBase {
-
-        createdBy: ICreatedBy;
-        dtChanged: Date;
-
-        constructor(entity: IEntityBaseArgs);
-
-        isValid(): boolean;
-        throwErrorIfIsInvalid(): void;
-    }
-    export function validateEmail(email): boolean;
+    isValid(): boolean;
+    throwErrorIfIsInvalid(): void;
 }
 
-declare module "ptz-core-domain"
-{
-    export = ptzCoreDomain;
+class EntityBase implements IEntityBase {
+
+    createdBy: ICreatedBy;
+    dtChanged: Date;
+
+    constructor(entity: IEntityBaseArgs);
+
+    isValid(): boolean;
+    throwErrorIfIsInvalid(): void;
 }
+
+export function validateEmail(email): boolean;
