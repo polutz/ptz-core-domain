a6e2438926dcb15912e18554243922b6895f39eb
Alan Marcell <alanmarcell@live.com>
2017-06-26 21:38:50 -0300

    Add index tests

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { ok } from 'ptz-assert';
+import * as index from './index';
+
+describe('ptz-core-domain', () => {
+    describe('exports', () => {
+        it('generateId', () => ok(index.generateId));
+        it('generateIdWith', () => ok(index.generateIdWith));
+    });
+});
